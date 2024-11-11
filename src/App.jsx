b2846e7d2a202bf052d5803554666286557219ee
DevDarji16 <devdarji033@gmail.com>
2024-11-11 23:34:58 +0530

    added rockpaperscissor

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Nav from './Components/Nav/Nav'
 import SlotMachine from './Components/slotmachine/SlotMachine'
 import Cards from './Components/Cards/Cards'
 import DiceGame from './Components/Dicegame/DiceGame'
+import RockPaper from './rockpaper/RockPaper'
 const App = () => {
     const [textColor,setTextColor]=useState('white')
     const [backgroundColor,setBackgroundColor]=useState('black')
@@ -17,6 +18,7 @@ const App = () => {
     const handleBack=()=>{
       setGame(null)
     }
+    
   return (
     <div className='maindiv' >
       <Nav/>
@@ -33,17 +35,24 @@ const App = () => {
                 <h1 style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>Dice Game</h1>
                 <p style={{display:'flex',justifyContent:'center',fontFamily:'monument',color:'pink'}}>Click to play</p>
             </div>
+            <div className='dicegame' onClick={()=>handleClick('rockpaper')} style={{height:'320px',backgroundColor:'gray' ,margin:'10px',userSelect:'none'  }}>
+                <img src="https://i.postimg.cc/0Qhrv6wp/Dice.jpg" alt="" style={{height:'200px ',width:'300px',objectFit:'cover'}}/>
+                <h1 style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>Rock Paper</h1>
+                <p style={{display:'flex',justifyContent:'center',fontFamily:'monument',color:'pink'}}>Click to play</p>
+            </div>
         </div>
+        
       )}
       {game &&(
         <div >
-          {game==='slotmachine'?<SlotMachine/>:<DiceGame/>}
+          {game==='slotmachine'?<SlotMachine/>:game==='dicegame'?<DiceGame/>:game==='rockpaper'?<RockPaper/>:null}
+         
         <button onClick={handleBack}>Go back</button>
         </div>
       )}
 
       
-      
+     
       
     </div>
   )
---
+++ src/rockpaper/RockPaper.jsx
@@ -0,0 +1,55 @@
+import React, { useState ,useEffect} from 'react'
+
+const RockPaper = () => {
+  const rps=['✊','✌️','✋']
+  const [score,setScore]=useState(0)
+  const [compare,setCompare]=useState(null)
+  const [random,setRandom]=useState(null)
+  const [result,setResult]=useState(null)
+  const randomGenerator=()=>{
+    const random=Math.floor(Math.random()*3)
+    return random
+  }
+  const handleClick=(a)=>{
+   setRandom(randomGenerator())
+   setCompare(a)
+  
+  }
+  useEffect(() => {
+    if (compare !== null && random !== null) {
+      handleResult();
+    }
+  }, [compare, random]);
+  const handleResult=()=>{
+    if((compare==='rock' && random===0)||(compare==='scissor' && random===1)||(compare==='paper' && random===2)){
+      setResult('Draw')
+    }
+    else if((compare==='rock'&&random===1)||(compare==='scissor'&&random===2)||(compare==='paper'&&random===0)){
+      setScore(score+1)
+      setResult('You win')
+    }
+    else{
+      setResult('You lose')
+    }
+  }
+  return (
+    <div>
+
+    <h1 style={{fontFamily:'monument',fontSize:'100px'}}>{random!==null?rps[random]:''}</h1>
+      
+    <h1 style={{fontFamily:'monument'}}>{result}</h1>
+
+      <h1 style={{fontFamily:'monument'}}>Press on any of the three</h1>
+      <div style={{display:'flex',gap:'20px'}}>
+      <div onClick={()=>handleClick('rock')} className='rock' style={{borderRadius:'20px',backgroundColor:'#313131',width:'200px',fontSize:'100px',height:'200px',display:'flex',justifyContent:'center',alignItems:'center',userSelect:'none'}}>✊</div>
+      <div onClick={()=>handleClick('scissor')} className='scissor' style={{borderRadius:'20px',backgroundColor:'#313131',width:'200px',fontSize:'100px',height:'200px',display:'flex',justifyContent:'center',alignItems:'center',userSelect:'none'}}>✌️</div>
+      <div onClick={()=>handleClick('paper')} className='paper' style={{borderRadius:'20px',backgroundColor:'#313131',width:'200px',fontSize:'100px',height:'200px',display:'flex',justifyContent:'center',alignItems:'center',userSelect:'none'}}>✋</div>
+      </div>
+      
+      <h1 style={{fontFamily:'monument'}}>Score:{score}</h1>
+      
+    </div>
+  )
+}
+
+export default RockPaper
