b10edca6094eca21cf58a2208435359951be3064
DevDarji16 <devdarji033@gmail.com>
2024-11-13 18:07:33 +0530

    wrapped

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,14 @@ import DiceGame from './Components/Dicegame/DiceGame'
 import RockPaper from './rockpaper/RockPaper'
 import Roulette from './Components/Roulette/Roulette'
 const App = () => {
+
+  const carddetails=[
+    {name:'Slot machine', component:'slotmachine',image:'https://i.postimg.cc/7Z50rsqj/Slotmachine.jpg'},
+    {name:'Slot machine', component:'slotmachine',image:'https://i.postimg.cc/7Z50rsqj/Slotmachine.jpg'},
+    {name:'Dice Game', component:'dicegame',image:'https://i.postimg.cc/0Qhrv6wp/Dice.jpg'},
+    {name:'Rock Paper', component:'rockpaper',image:'https://i.postimg.cc/PJx2t2s3/rockpaper.jpg'},
+    {name:'Roulette', component:'rockpaper',image:'https://i.postimg.cc/sXBFJ80F/roulette.jpg'},
+  ]
     const [textColor,setTextColor]=useState('white')
     const [backgroundColor,setBackgroundColor]=useState('black')
     const[game,setGame]=useState(null)
@@ -26,19 +34,23 @@ const App = () => {
       <Nav tscore={tscore} setTScore={setTScore} setGame={setGame}/>
       {!game &&(
         <div >
-          <div className='Cards' style={{display:'flex'}}>
-             <div className='slotmachine' onClick={()=>handleClick('slotmachine')} style={{height:'320px',backgroundColor:'gray' ,margin:'10px',userSelect:'none'  }}>
-                <img src="https://i.postimg.cc/7Z50rsqj/Slotmachine.jpg" alt="" style={{height:'200px'}}/>
-                <h1 style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>Slot Game</h1>
+          <div className='Cards' style={{display:'flex',flexWrap:'wrap'}}>
+          {carddetails.map((a,index)=>(
+            <div key={index} className={a.component} onClick={()=>handleClick('slotmachine')} style={{height:'320px',backgroundColor:'gray' ,margin:'10px',userSelect:'none' ,width:'300px' }}>
+                <img src={a.image} alt="" style={{height:'200px',width:'300px',objectFit:'cover'}}/>
+                <h1 style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>{a.name}</h1>
                 <p style={{display:'flex',justifyContent:'center',fontFamily:'monument',color:'pink'}}>Click to play</p>
             </div>
-            <div className='slotmachine' onClick={()=>handleClick('')} style={{height:'320px',backgroundColor:'gray' ,margin:'10px',userSelect:'none'  }}>
+          ))}
+          
+             
+            {/* <div className='slotmachine' onClick={()=>handleClick('')} style={{height:'320px',backgroundColor:'gray' ,margin:'10px',userSelect:'none'  }}>
                 <img src="https://i.postimg.cc/7Z50rsqj/Slotmachine.jpg" alt="" style={{height:'200px'}}/>
                 <h1 style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>Slot Game </h1>
                 <p style={{marginTop:'-20px',fontFamily:'monument',textAlign:'center'}}>(Filtered)</p>
                 <p style={{marginTop:'-10px',display:'flex',justifyContent:'center',fontFamily:'monument',color:'pink'}}>Click to play</p>
             </div>
-            {/* Dice game */}
+            
             <div className='dicegame' onClick={()=>handleClick('dicegame')} style={{height:'320px',backgroundColor:'gray' ,margin:'10px',userSelect:'none'  }}>
                 <img src="https://i.postimg.cc/0Qhrv6wp/Dice.jpg" alt="" style={{height:'200px ',width:'300px',objectFit:'cover'}}/>
                 <h1 style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>Dice Game</h1>
@@ -48,15 +60,17 @@ const App = () => {
                 <img src="https://i.postimg.cc/PJx2t2s3/rockpaper.jpg" alt="" style={{height:'200px ',width:'300px',objectFit:'cover'}}/>
                 <h1 style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>Rock Paper</h1>
                 <p style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>Click to play</p>
-            </div>
+            </div> */}
           </div>
-          <div className='Cards' style={{display:'flex'}}>
+
+
+          {/* <div className='Cards' style={{display:'flex'}}>
           <div className='rockpaper' onClick={()=>handleClick('roulette')} style={{height:'320px',backgroundColor:'gray' ,margin:'10px',userSelect:'none'  }}>
                 <img src="https://i.postimg.cc/sXBFJ80F/roulette.jpg" alt="" style={{height:'200px ',width:'300px',objectFit:'cover'}}/>
                 <h1 style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>Roulette</h1>
                 <p style={{display:'flex',justifyContent:'center',fontFamily:'monument'}}>Click to play</p>
             </div>
-          </div>
+          </div> */}
             
             
         </div>
